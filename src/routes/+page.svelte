62242c23e05d45c210422ce9ba7f6153750b6ee3
jovany velez <jovany.velez@udea.edu.co>
2023-10-09 17:29:21 -0500

    mod9

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,86 +1,81 @@
 <script>
-    import { Html5Qrcode } from 'html5-qrcode'
-    import { onMount } from 'svelte'
+	import { Html5Qrcode } from 'html5-qrcode';
+	import { onMount } from 'svelte';
 
-    let scanning = false
+	let scanning = false;
 
-    let html5Qrcode
+	let html5Qrcode;
 
-    let codigo = "";
+	let codigo = '';
 
-    onMount(init)
+	onMount(init);
 
-    function init() {
-        html5Qrcode = new Html5Qrcode('reader')
-    }
+	function init() {
+		html5Qrcode = new Html5Qrcode('reader');
+	}
 
-    function start() {
-        html5Qrcode.start(
-            { facingMode: 'environment' },
-            {
-                fps: 10,
-                qrbox: { width: 250, height: 250 },
-            },
-            onScanSuccess,
-            onScanFailure
-        )
-        scanning = true
-    }
+	function start() {
+		html5Qrcode.start(
+			{ facingMode: 'environment' },
+			{
+				fps: 10,
+				qrbox: { width: 250, height: 250 }
+			},
+			onScanSuccess,
+			onScanFailure
+		);
+		scanning = true;
+	}
 
-    async function stop() {
-        await html5Qrcode.stop()
-        scanning = false
-    }
+	async function stop() {
+		await html5Qrcode.stop();
+		scanning = false;
+	}
 
-    async function onScanSuccess(decodedText, decodedResult) {
-        //alert(`Code matched = ${decodedText}`)
-        codigo = decodedText
-        console.log(decodedResult)
-        await html5Qrcode.stop()
-        scanning = false
+	async function onScanSuccess(decodedText, decodedResult) {
+		//alert(`Code matched = ${decodedText}`)
+		codigo = decodedText;
+		console.log(decodedResult);
+		await html5Qrcode.stop();
+		scanning = false;
+	}
 
-    }
-
-    function onScanFailure(error) {
-        console.warn(`Code scan error = ${error}`)
-    }
+	function onScanFailure(error) {
+		console.warn(`Code scan error = ${error}`);
+	}
 </script>
 
-<style>
-    main {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        gap: 20px;
-    }
-    reader {
-        width: 100%;
-        min-height: 300px;
-        background-color: black;
-    }
-</style>
-
 <main>
-    <h1>El FINO</h1>
-    <h1  style="color:red">{Math.floor((Math.random() * (20000 - 500 + 1)) + 500)}</h1>
-    {#if scanning}
-        <button on:click={stop}>Detener</button>
-    {:else}
-        <button on:click={start}>Ver precio</button>
-    {/if}
-    <reader id="reader"/>
-    <h3>
-        {codigo}
-    </h3>
+	<h1>El FINO</h1>
+    {#key codigo}
+	    <h1 style="color:red">{Math.floor(Math.random() * (20000 - 500 + 1) + 500)}</h1>
+    {/key}
+	{#if scanning}
+		<button on:click={stop}>Detener</button>
+	{:else}
+		<button on:click={start}>Ver precio</button>
+	{/if}
+	<reader id="reader" />
+	{#key codigo}
+		<h3>
+			{codigo}
+		</h3>
+	{/key}
 
-    <h2>
-        El precio es:
-    </h2>
-
-
+	<h2>El precio es:</h2>
 </main>
 
-
-
-
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: 20px;
+	}
+	reader {
+		width: 100%;
+		min-height: 300px;
+		background-color: black;
+	}
+</style>
